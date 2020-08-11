fb6ab3f0ce6d88148bf1d9990c841a6403ab96b7
OraSum <fatelucifer@foxmail.com>
2020-08-11 18:15:38 +0800

    3.3.2
    新增检索工具跳转入口

--- pages/info/info.js
+++ pages/info/info.js
@@ -1,4 +1,5 @@
 // pages/info/info.js
+const APP = getApp();
 
 Page({
 
@@ -6,13 +7,55 @@ Page({
    * 页面的初始数据
    */
   data: {
-    items: [
-      { interval: false, type: 'nav', text: '恶名精英图鉴', icon: 'nm', src: '/pages/nm/nm' },
-      { interval: false, type: 'nav', text: '禁地兵装', icon: 'weapon', src: '/pages/weapon/weapon' },
-      { interval: false, type: 'nav', text: '优雷卡天气预报', icon: 'weather', src: '/pages/weather/weather' },
-      { interval: false, type: 'nav', text: '文理碎晶', icon: 'crystal_light', src: '/pages/brokenCrystal/brokenCrystal' },
-      { interval: true, type: 'btn', text: '清除缓存', icon: 'clear', src: 'clearStorage' },
-      { interval: false, type: 'nav', text: '关于', icon: 'info', src: '/pages/logs/logs' },
+    items: [{
+        interval: false,
+        type: 'nav',
+        text: '恶名精英图鉴',
+        icon: 'nm',
+        src: '/pages/nm/nm'
+      },
+      {
+        interval: false,
+        type: 'nav',
+        text: '禁地兵装',
+        icon: 'weapon',
+        src: '/pages/weapon/weapon'
+      },
+      {
+        interval: false,
+        type: 'nav',
+        text: '优雷卡天气预报',
+        icon: 'weather',
+        src: '/pages/weather/weather'
+      },
+      {
+        interval: false,
+        type: 'nav',
+        text: '文理碎晶',
+        icon: 'crystal_light',
+        src: '/pages/brokenCrystal/brokenCrystal'
+      },
+      {
+        interval: false,
+        type: 'miniprogram',
+        text: 'FF14检索工具 - 工具合集',
+        icon: 'icon-tool',
+        src: 'wx895a7377514cce46'
+      },
+      {
+        interval: true,
+        type: 'btn',
+        text: '清除缓存',
+        icon: 'clear',
+        src: 'clearStorage'
+      },
+      {
+        interval: false,
+        type: 'nav',
+        text: '关于',
+        icon: 'info',
+        src: '/pages/logs/logs'
+      },
     ]
   },
   onTapItem(e) {
@@ -25,6 +68,17 @@ Page({
         break;
       case 'btn':
         this.runFunc(this.data.items[index].src);
+        break;
+      case 'miniprogram':
+        if (APP.globalData.env === 'wechat') {
+          wx.navigateToMiniProgram({
+            appId: this.data.items[index].src
+          });
+        } else {
+          qq.navigateToMiniProgram({
+            appId: '1110321393'
+          });
+        }
         break;
       default:
         break;
@@ -47,5 +101,12 @@ Page({
         icon: 'none'
       });
     }
+  },
+  handleTapHabbit() {
+    wx.showToast({
+      title: '宝藏好像在东北方向很远的地方……',
+      duration: 5000,
+      icon: 'none'
+    });
   }
 })
